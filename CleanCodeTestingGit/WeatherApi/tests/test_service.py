af7a1ee3b10379e526185927a719723b2e787e88
Michelle-anyika <m.anyika@alustudent.com>
2026-03-26 12:50:30 +0200

    test: add failing test for provider

--- CleanCodeTestingGit/WeatherApi/tests/test_service.py
+++ CleanCodeTestingGit/WeatherApi/tests/test_service.py
@@ -17,3 +17,16 @@ def test_invalid_api_key():
     with pytest.raises(InvalidAPIKeyError):
         service.get_forecast("Kigali")
 
+def test_service_uses_provider(mocker):
+    mock_provider = mocker.Mock()
+    mock_provider.fetch.return_value = {
+        "temperature": 99,
+        "condition": "Storm"
+    }
+
+    service = WeatherService(provider=mock_provider)
+
+    result = service.get_forecast("Kigali")
+
+    mock_provider.fetch.assert_called_once_with("kigali")
+    assert result["temperature"] == 99
