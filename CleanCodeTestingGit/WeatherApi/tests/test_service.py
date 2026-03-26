d203bc2250269eb2e71e611aa4eb92cb7cfef182
Michelle-anyika <m.anyika@alustudent.com>
2026-03-26 12:41:37 +0200

    feat: validate API key in WeatherService

--- CleanCodeTestingGit/WeatherApi/tests/test_service.py
+++ CleanCodeTestingGit/WeatherApi/tests/test_service.py
@@ -1,7 +1,7 @@
 import pytest
 from ..src.weather.service import WeatherService
 from ..src.weather.exceptions import CityNotFoundError
-
+from ..src.weather.exceptions import InvalidAPIKeyError
 @pytest.fixture
 def weather_service():
     return WeatherService()
@@ -9,3 +9,11 @@ def weather_service():
 def test_get_forecast_unknown_city(weather_service):
     with pytest.raises(CityNotFoundError):
         weather_service.get_forecast("paris")
+
+
+def test_invalid_api_key():
+    service = WeatherService(api_key="bad-key")
+
+    with pytest.raises(InvalidAPIKeyError):
+        service.get_forecast("Kigali")
+
